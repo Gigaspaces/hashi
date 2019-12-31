ec0757e7f82f9aa01667454bf364606542faac34
yohanakh <yohana.khoury@gmail.com>
2019-12-31 14:08:20 +0200

    fix user-data for client: change log folder + fix call to consul cmd

--- provision/spotinst/client/user-data.sh
+++ provision/spotinst/client/user-data.sh
@@ -8,12 +8,13 @@ cd /data/
 if [ ! -e "hashi" ]; then
     git clone https://github.com/GigaSpaces/hashi.git
     cd hashi/provision/installation
+    mkdir /tmp/hashi-logs
 
-    ./install.sh > logs/install.out
+    ./install.sh > /tmp/hashi-logs/install.out
 
-    ./consul/install_consul.sh client "172.31.5.119" > logs/install_consul.out
-    ./consul join "13.59.22.153" > logs/consul_join.out
-    ./nomad/install_nomad.sh client > logs/install_nomad.out
+    ./consul/install_consul.sh client "172.31.5.119" > /tmp/hashi-logs/install_consul.out
+    consul join "13.59.22.153" > /tmp/hashi-logs/consul_join.out
+    ./nomad/install_nomad.sh client > /tmp/hashi-logs/install_nomad.out
 
 
 fi
